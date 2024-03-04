e64c05d1176b333bd60321b9008919790530042e
Haris Volos <hvolos01@ucy.ac.cy>
2024-03-04 10:05:07 +0000

    Update mixed-workload_type_1.lua

--- assignments/hw3/hotelapp/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua
+++ assignments/hw3/hotelapp/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua
@@ -109,10 +109,10 @@ end
 
 request = function()
   cur_time = math.floor(socket.gettime())
-  local search_ratio      = 0.6
-  local recommend_ratio   = 0.39
-  local user_ratio        = 0.005
-  local reserve_ratio     = 0.005
+  local search_ratio      = 1.0
+  local recommend_ratio   = 0.0
+  local user_ratio        = 0.0
+  local reserve_ratio     = 0.0
 
   local coin = math.random()
   if coin < search_ratio then
